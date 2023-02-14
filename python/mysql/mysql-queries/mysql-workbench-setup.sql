776084415372a83095e0d7ae85609a22a51d08ab
Reece Hunter <heecerunter@gmail.com>
2023-02-14 13:32:34 -0600

    .

--- python/mysql/mysql-queries/mysql-workbench-setup.sql
+++ python/mysql/mysql-queries/mysql-workbench-setup.sql
@@ -1,3 +1,6 @@
 INSERT INTO names VALUES (1, "Reece", current_time(), current_time());
 DELETE FROM names WHERE id = 1;
 INSERT INTO names VALUES (1, "Reece", current_time(), current_time()), (2, "Hunter", current_time(), current_time());
+UPDATE names SET name = "Alfred" WHERE id = 1;
+DELETE FROM names WHERE id = 1;
+DELETE FROM names WHERE id = 2;
