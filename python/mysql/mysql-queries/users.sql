ebf51f15c2f3acb57691ca0f3bb4373d77b9d552
Reece Hunter <heecerunter@gmail.com>
2023-02-14 14:05:55 -0600

    .

---
+++ python/mysql/mysql-queries/users.sql
@@ -0,0 +1,18 @@
+USE users_schema;
+
+INSERT INTO users
+VALUES
+(1, "Reece", "Hunter", "reece@mail.gom", current_time(), current_time()),
+(2, "Bob", "Bobson", "bob@mail.gom", current_time(), current_time()),
+(3, "Phil", "Bill", "phil@mail.gom", current_time(), current_time());
+
+SELECT * FROM users;
+
+SELECT * FROM users WHERE email = "reece@mail.gom";
+
+SELECT * FROM users WHERE id = 3;
+
+UPDATE users SET last_name = "Pancakes" WHERE id = 3;
+
+DELETE FROM users WHERE id = 2;
+
