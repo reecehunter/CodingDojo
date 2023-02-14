365fc247c98f5fea2c9f87db73f2cf90a8f0f8bd
Reece Hunter <heecerunter@gmail.com>
2023-02-14 13:29:08 -0600

    .

--- python/mysql/mysql-queries/mysql-workbench-setup.sql
+++ python/mysql/mysql-queries/mysql-workbench-setup.sql
@@ -1,1 +1,3 @@
-INSERT INTO names VALUES (1, "Reece", current_time(), current_time())
+INSERT INTO names VALUES (1, "Reece", current_time(), current_time());
+DELETE FROM names WHERE id = 1;
+INSERT INTO names VALUES (1, "Reece", current_time(), current_time()), (2, "Hunter", current_time(), current_time());
