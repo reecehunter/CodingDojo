1be2a6d573c5abac851a0cf43d9aa624dff6e4e6
reecehunter <heecerunter@gmail.com>
2023-04-05 19:24:01 -0600

    .

---
+++ java/java-oop/sts-workspace/CarAndDriver/src/Car.java
@@ -0,0 +1,12 @@
+
+public class Car {
+	public int gas;
+	
+	public Car() {
+		gas = 10;
+	}
+	
+	public void displayGas() {
+		System.out.println("Gas: " + gas);
+	}
+}
---
+++ java/java-oop/sts-workspace/CarAndDriver/src/CarAndDriver.java
@@ -0,0 +1,15 @@
+
+public class CarAndDriver {
+	public static void main(String[] args) {
+		Driver driver1 = new Driver();
+		
+		driver1.drive();
+		driver1.drive();
+		driver1.drive();
+		driver1.drive();
+		driver1.boost();
+		driver1.refuel();
+
+		driver1.displayGas();
+	}
+}
